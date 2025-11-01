da61d512484863c80b6f7d79ae06d77bf085a1d2
RyotaNakahara <rita.n4210@gmail.com>
2025-11-02 08:55:49 +0900

    chore: アプリケーションの骨格を作成

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Expense Tracker App</h1>
+      <p>Welcome to your expense tracker application!</p>
+    </div>
+  )
+}
+
+export default Home
+
